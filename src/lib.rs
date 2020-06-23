cdae6ed2d47eb4b335251db22cc69032a4c82a6c
Tuhin Nair <tuhinnair444@gmail.com>
2020-06-23 10:16:04 -0700

    minigrep v1

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    pub query: String,
+    pub filename: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Self, &str> {
+        if args.len() < 3 {
+            Err("Not enough arguments")
+        } else {
+            let query: String = args[1].clone();
+            let filename: String = args[2].clone();
+            Ok(Self { query, filename })
+        }
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    for line in search(&config.query, &contents) {
+        println! {"\nFound in : {}", line}
+    }
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn found() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+use std::process;
+
+mod lib;
+use lib::Config;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|e| {
+        eprintln!("{}", e);
+        process::exit(1);
+    });
+
+    println!(
+        "Searching for \"{}\" in file \"{}\"",
+        config.query, config.filename
+    );
+
+    if let Err(e) = lib::run(config) {
+        eprintln!("Application Error: {}", e);
+        process::exit(1);
+    }
 }
